f12e3efba549478bfd169c3e20a5c30f096ec639
reyna garcia <garcias.reyna@gmail.com>
2020-11-18 22:26:52 -0500

    added event to submit button

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,1 +1,13 @@
-console.log(" Probando !")
+$(document).ready(function () {
+    // console.log("probando ready")
+    // link submit button
+    // add event to submit
+    // link textbox to take burger data
+    // call API to send burger data
+    var submitButton = $("#submit-button")
+    var textBox = $("#text-input")
+    submitButton.on("click", function () {
+        console.log("clicked")
+    })
+
+});     
